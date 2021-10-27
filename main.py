6b9ce5d0a1418b402902d14468b89c89f0db022a
fdeodato11 <fdeodato11@gmail.com>
2021-10-27 20:46:03 -0300

    adiciona estrutura de imagens juntamente com primeiro inimigo
    Signed-off-by: fdeodato11 <fdeodato11@gmail.com>

--- main.py
+++ main.py
@@ -22,11 +22,13 @@ def draw_bg():
   screen.fill(BG)
 
 
-class Wood(pygame.sprite.Sprite):
-  def __init__(self, x, y, scale, speed):
+class Character(pygame.sprite.Sprite):
+  def __init__(self, char_type, x, y, scale, speed):
     pygame.sprite.Sprite.__init__(self)
     self.speed = speed
-    img = pygame.image.load('images/wood/wood.png')
+    self.direction = 1
+    self.flip = False
+    img = pygame.image.load(f'images/{char_type}/idle/0.png')
     self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height()* scale)))
     self.rect = self.image.get_rect()
     self.rect.center = (x, y)
@@ -37,17 +39,22 @@ class Wood(pygame.sprite.Sprite):
 
     if moving_left:
       dx = -self.speed
+      self.flip = True
+      self.direction = -1
     if moving_rigth:
       dx = self.speed
+      self.flip = False
+      self.direction = 1
 
     self.rect.x += dx
     self.rect.y += dy
 
   
   def draw(self):
-    screen.blit(self.image, self.rect)
+    screen.blit(pygame.transform.flip(self.image ,self.flip, False), self.rect)
 
-player = Wood(200, 200, 0.3, 5)
+player = Character('wood' ,200, 200, 0.4, 5)
+enemy = Character('guarda3' ,400, 200, 0.1, 5)
 
 
 run = True
@@ -58,6 +65,7 @@ while run:
   draw_bg()
 
   player.draw()
+  enemy.draw()
   player.move(moving_left, moving_rigth)
 
   for event in pygame.event.get():
